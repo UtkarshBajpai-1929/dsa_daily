aa831b5fea36098067a38ac5195a432d84445ea2
Utkarsh Bajpai <utkarshbajpai632@gmail.com>
2026-07-22 11:19:43 +0530

    Time: 3 ms (70.81%) | Memory: 18.6 MB (39.57%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,49 +1,45 @@
 class Solution {
 public:
-      bool isCycle(int curr, vector<bool> &vis, vector<bool> &rPath, vector<vector<int>> &prerequisites){
+      bool isCycle(int curr, vector<bool> &vis, vector<bool> &rPath, vector<vector<int>> &adj){
         vis[curr] = true;
         rPath[curr] = true;
 
-        for(int i=0; i<prerequisites.size(); i++){
-            int u = prerequisites[i][1];
-            int v = prerequisites[i][0];
-
-            if(curr == u){
+        for(int v: adj[curr]){
                 if(!vis[v]){
-                    if(isCycle(v, vis, rPath, prerequisites)){
+                    if(isCycle(v, vis, rPath, adj)){
                         return true;
                     }
                 }else if(rPath[v]){
                         return true;
                     }
-            }
         }
         rPath[curr] = false;
         return false;
     }
 
-    void order(int src, vector<bool> &vis, vector<vector<int>> &prerequisites, stack<int> &st){
+    void order(int src, vector<bool> &vis, vector<vector<int>> &adj, stack<int> &st){
         vis[src] = true;
-        for(int i=0; i<prerequisites.size(); i++){
-            int u = prerequisites[i][1];
-            int v = prerequisites[i][0];
-
-            if(src==u){
+        for(int v: adj[src]){
                 if(!vis[v]){
-                    order(v, vis, prerequisites, st);
+                    order(v, vis, adj, st);
                 }
-            }
         }
         st.push(src);
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        for(int i=0; i<prerequisites.size(); i++){
+            int u = prerequisites[i][1];
+            int v = prerequisites[i][0];
+            adj[u].push_back(v);
+        }
         int n = numCourses;
         vector<bool> vis(n, false);
         vector<bool>rPath(n, false);
         stack<int> st;
         for(int i=0; i<n; i++){
             if(!vis[i]){
-                if(isCycle(i, vis, rPath, prerequisites)){
+                if(isCycle(i, vis, rPath, adj)){
                     return {};
                 }
             }
@@ -51,7 +47,7 @@ public:
         fill(vis.begin(), vis.end(), false);
         for(int i=0; i<n; i++){
             if(!vis[i]){
-                order(i, vis, prerequisites,st);
+                order(i, vis, adj,st);
             }
         }
         vector<int> ans;
