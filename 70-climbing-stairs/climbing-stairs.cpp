bf52b8fe1bd8e7d7f330aa166e9ef0877d28bcda
Utkarsh Bajpai <utkarshbajpai632@gmail.com>
2026-08-04 08:18:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (74.46%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1 || n==2) return n;
+        int ans = 0;
+        int prev1 = 1;
+        int prev2 = 2;
+        for(int i=3; i<=n; i++){
+            ans = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = ans;
+        }
+        return ans;
+    }
+};
